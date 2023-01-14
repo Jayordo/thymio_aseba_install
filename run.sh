7881628a0d8793a1c3b6dff50d301286c201b9bb
Jayordo <jeroenheijstek@hotmail.com>
2023-01-14 19:06:04 +0000

    avoid objects

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ wait
 
 # asebamedulla "ser:device=/dev/ttyACM0"
 # asebamedulla "ser:device=/dev/ttyAMA0"
+# sudo service dbus restart
 asebamedulla "ser:name=Thymio-II" &
 python controller.py
+kill $!
