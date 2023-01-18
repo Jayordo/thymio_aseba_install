5d83485dfcce913c3e4cbc5e3b1acc5a2ada1f4c
jayordo <jeroenheijstek@hotmail.com>
2023-01-18 14:22:21 +0100

    remove old clutter added 32bit install and video test script

--- run.sh
+++
@@ -1,9 +0,0 @@
-git pull &
-wait
-
-# asebamedulla "ser:device=/dev/ttyACM0"
-# asebamedulla "ser:device=/dev/ttyAMA0"
-# sudo service dbus restart
-asebamedulla "ser:name=Thymio-II" &
-python controller.py
-kill $!
