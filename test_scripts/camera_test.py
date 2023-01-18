e5f4fe44fe46af6fc0200ff7ea07f1550c7a5291
jayordo <jeroenheijstek@hotmail.com>
2023-01-18 21:30:25 +0100

    camera test now loops

--- test_scripts/camera_test.py
+++ test_scripts/camera_test.py
@@ -2,8 +2,18 @@ import cv2
 import climage
 
 vid = cv2.VideoCapture(0)
-ret, frame = vid.read()
-cv2.imwrite('/tmp/frame.png', frame)
-output = climage.convert('/tmp/frame.png')
-print(output)
+while True:
+    # Capture the video frame by frame
+    ret, frame = vid.read()
+    # function to resize
+    # frame = cv2.resize(frame, (700, 500))
+
+    # Display the resulting frame
+    cv2.imwrite('/tmp/frame.png', frame)
+    output = climage.convert('/tmp/frame.png')
+    print(output)
+    # the 'q' button is set as the quitting button you may use any desired button of your choice
+    if cv2.waitKey(1000) & 0xFF == ord('q'):
+        break
+# After the loop release the cap object
 vid.release()
