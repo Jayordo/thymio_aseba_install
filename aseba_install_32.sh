91fd4cc5754f1bd0a1754b93af4249415a2425b2
jayordo <jeroenheijstek@hotmail.com>
2023-01-18 17:07:00 +0100

    clean test 1.3

--- aseba_install_32.sh
+++ aseba_install_32.sh
@@ -8,8 +8,9 @@ sudo flatpak install org.mobsya.ThymioSuite -y
 #requirements for opencv?
 #sudo apt install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
 
-sudo apt update --fix-missing
-sudo apt full-upgrade
+sudo apt update --fix-missing -y && apt upgrade -y
+#mb dont do this, it takes too long
+#sudo apt full-upgrade
 
 sudo pip3 install tdmclient
 
