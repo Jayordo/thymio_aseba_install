da76dc0815ddb5664a77a8a3486bb8358055bbdd
Jeroen Heijstek <jeroenheijstek@hotmail.com>
2023-01-18 15:20:21 +0100

    Update aseba_install_32.sh

--- aseba_install_32.sh
+++ aseba_install_32.sh
@@ -1,4 +1,4 @@
-Install instructions:
+#Install instructions:
 
 sudo apt full-upgrade -y
 
@@ -7,13 +7,16 @@ sudo apt install flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install org.mobsya.ThymioSuite -y
 
-pip install --upgrade -y tdmclient
-pip install -y opencv-python
+pip install tdmclient
+pip install opencv-python
+#After building the robots, the UI-less version can be used because this one takes loooong
+#pip install opencv-python-headless
+
 #optional
 #pip install -y thymiodirect
 
-echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000a", MODE="0666"' | sudo tee -a /etc/udev/rules.d/99-mobsya.rules
-echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000c", MODE="0666"' | sudo tee -a /etc/udev/rules.d/99-mobsya.rules
+#TODO: test if this is needed
+echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000a", MODE="0666"\nSUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000c", MODE="0666"' | sudo tee /etc/udev/rules.d/99-mobsya.rules
 sudo udevadm control --reload-rules
 
 #Test if its working
@@ -21,8 +24,7 @@ sudo udevadm control --reload-rules
 #python3 -m tdmclient list
 
 #https://raspberrypi-guide.github.io/programming/install-opencv.html
-#dunno how much of this is needed
+#This might speed up opencv install, but is a bit messy
 #sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
-#pip install opencv-python
 
 #TODO: add autostart to TDM
