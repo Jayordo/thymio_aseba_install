f03ff3e070c49e173f6abb3f627510776b73791d
jayordo <jeroenheijstek@hotmail.com>
2023-01-18 17:37:24 +0100

    clean test 1.4

--- aseba_install_32.sh
+++ aseba_install_32.sh
@@ -1,13 +1,12 @@
 #Install instructions:
 
+sudo apt update --fix-missing -y && apt upgrade -y
 sudo apt install xrdp -y
 sudo apt install flatpak -y
-sudo apt install python3-opencv -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install org.mobsya.ThymioSuite -y
 #requirements for opencv?
 #sudo apt install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
-
 sudo apt update --fix-missing -y && apt upgrade -y
 #mb dont do this, it takes too long
 #sudo apt full-upgrade
@@ -18,6 +17,8 @@ sudo pip3 install tdmclient
 #the link extracts a pre-built wheel which improves install speed massively
 
 #sudo pip3 install opencv-python
+#sudo pip install opencv-python==4.5.3.56
+#sudo pip install opencv-python==4.3.0.38
 
 #After developing the robots, the UI-less version can be used because this one takes loooong
 #sudo pip3 install opencv-python-headless
