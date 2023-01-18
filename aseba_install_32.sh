178bff41d5ef0973dbca4b5589556cb345037841
jayordo <jeroenheijstek@hotmail.com>
2023-01-18 18:21:58 +0100

    clean test 2.0

--- aseba_install_32.sh
+++ aseba_install_32.sh
@@ -1,36 +1,24 @@
 #Install instructions:
 
-sudo apt update --fix-missing -y && apt upgrade -y
+sudo apt-get update --fix-missing -y && sudo apt-get upgrade -y
 sudo apt install xrdp -y
+sudo apt install libatlas-base-dev -y
 sudo apt install flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install org.mobsya.ThymioSuite -y
-#requirements for opencv?
-#sudo apt install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
-sudo apt update --fix-missing -y && apt upgrade -y
-#mb dont do this, it takes too long
-#sudo apt full-upgrade
+
+echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000a", MODE="0666"\nSUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000c", MODE="0666"' | sudo tee /etc/udev/rules.d/99-mobsya.rules
+sudo udevadm control --reload-rules
 
 sudo pip3 install tdmclient
-
-#TODO: test if above reqs make the -i command unnecessary
-#the link extracts a pre-built wheel which improves install speed massively
-
-#sudo pip3 install opencv-python
-#sudo pip install opencv-python==4.5.3.56
-#sudo pip install opencv-python==4.3.0.38
-
-#After developing the robots, the UI-less version can be used because this one takes loooong
-#sudo pip3 install opencv-python-headless
+sudo pip3 install opencv-python==4.5.3.56
+#sudo pip3 install opencv-python-headless #for post dev use
+sudo pip3 install -U numpy
 
 #optional lib for direct serial usb connection
 #sudo pip3 install -y thymiodirect
 
-#TODO: test if this is needed
-#echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000a", MODE="0666"\nSUBSYSTEM=="usb", ATTRS{idVendor}=="0617", ATTRS{idProduct}=="000c", MODE="0666"' | sudo tee /etc/udev/rules.d/99-mobsya.rules
-#sudo udevadm control --reload-rules
-
-#Test if its working
+#Test if its working, it might need a reboot
 #flatpak run --command=thymio-device-manager org.mobsya.ThymioSuite
 #python -m tdmclient list
 
